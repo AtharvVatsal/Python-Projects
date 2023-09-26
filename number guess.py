a5ec7272b359d2f8b690734d304d84b88ed0ff13
Atharv Vatsal <atharvvatsal@outlook.com>
2023-09-26 21:45:55 +0530

    Number Guessing game, will be further modified

---
+++ number guess.py
@@ -0,0 +1,53 @@
+import random
+choice =int(input("""
+    1 for easy (1-10)
+    2 for medium (1-100)
+    3 for hard (1-1000)
+
+>>> """))
+if choice == 1:
+    count = 7
+    taken = 0
+    while taken <= count:
+        num = int(input("Enter Your Guess: "))
+        taken += 1
+        dig = random.randint(1, 10)
+        if num == dig:
+            print("Woila, You Got Right Number")
+            break
+        elif num < dig:
+            print("TOO LOW!")
+        else:
+            print("TOO HIGH")
+elif choice == 2:
+    count = 7
+    taken = 0
+    while taken <= count:
+        num = int(input("Enter Your Guess: "))
+        taken += 1
+        dig = random.randint(11, 100)
+        if num == dig:
+            print("Woila, You Got Right Number")
+            break
+        elif num < dig:
+            print("TOO LOW!")
+        else:
+            print("TOO HIGH")
+    while taken == count:
+        print(f"The Number was {dig}")
+elif choice == 3:
+    count = 7
+    taken = 0
+    while taken <= count:
+        num = int(input("Enter Your Guess: "))
+        taken += 1
+        dig = random.randint(1, 1000)
+        if num == dig:
+            print("Woila, You Got Right Number")
+            break
+        elif num < dig:
+            print("TOO LOW!")
+        else:
+            print("TOO HIGH")
+else:
+    print("Invalid Choice, Re-run")
